1d03a2c068e606d5793c64ec718e5e0338427cdf
DarkblooM-IO <darkbloompro@gmail.com>
2025-06-01 11:10:55 +0200

    commit

---
+++ scripts/launch_polybar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# script by apetresc
+# https://github.com/polybar/polybar/issues/763#issuecomment-331604987
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
